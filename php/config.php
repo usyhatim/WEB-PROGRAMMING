e8b5dcbded71753ad5f62ee6f2741b71c3796deb
Zaky <116427394+jamky123@users.noreply.github.com>
2026-06-05 00:20:56 +0800

    Clear database credentials for security reason  in config.php
    Removed sensitive database credentials from config.php.

--- php/config.php
+++ php/config.php
@@ -2,8 +2,8 @@
 
 $host = "localhost";
 $user = "newsdb"; // Change this to your MySQL username
-$password = "SECV_2223"; // Change this to your MySQL password
-$database = "newsdb"; // Change this to your MySQL database name
+$password = ""; // Change this to your MySQL password
+$database = ""; // Change this to your MySQL database name
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
